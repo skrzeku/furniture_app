b3fd419ca8da7885a4efc961c8ba2e1eb337f72e
skrzeku <skucha1@wp.pl>
2017-11-19 17:01:22 +0100

    1911badcommit

--- js/forms.js
+++ js/forms.js
@@ -1,4 +1,16 @@
- function function1 () {
+var background = "#backgrounds";
+var w_value = "#width_value";
+var h_value = "#height_value";
+var sub = "#submit";
+var sub2 = "#submit2";
+var sub3 = "#submit3";
+var sub4 = "#submit4";
+var sub5 = "#submit5";
+var sub6 = "#submit6";
+var elements_form = "#elements_form";
+var click_wstaw = 0;
+
+function function1 () {
                 var x = document.getElementById("myForm").elements.item(0).value;
                 var y = document.getElementById("myForm").elements.item(1).value;
                 if (x != "" && y != "") {
@@ -18,6 +30,7 @@
                             $("#submit").fadeOut(1500);
                             $("#first_span").fadeOut(1500, function() {
                                 $("#submit2").fadeIn(500);
+                                $("#submit6").fadeIn(500);
                             });
                             
                             
@@ -40,13 +53,65 @@
                 
             };
 function add_elements () {
-    $("#submit2").fadeOut(1000, function (){
-        $("#select_element").fadeIn(1000);
-        $("#submit4").fadeIn(1000);
-    });
+    $(sub2).fadeOut(1000);
+        $(sub6).fadeOut(1000, function (){
+            $("#select_element").fadeIn(2000);
+        $(sub4).fadeIn(2000);
+        });
+        
 };
 function accept_direction () { 
 var select_direction = $( "#select_element option:selected" ).val();
-    alert(select_direction);
+    if(select_direction == "hori_direct") {
+        $("#select_element").fadeOut(1000);
+        $(elements_form).fadeIn(1000);
+        $(sub4).fadeOut(1000, function () {
+            $(sub5).fadeIn(1000);
+        });
+        
+        
+    }
+    else {
+        $("#select_element").fadeOut(1000);
+        $(elements_form).fadeIn(1000);
+        $(sub4).fadeOut(1000, function () {
+            $(sub5).fadeIn(1000);
+        });
+        
+    }
 
 };
+function add_ele() {
+    
+               click_wstaw++;
+                var elements = $( "#elements_form option:selected" ).val();
+                var val1_el = $("#width_el1").val();
+                var val2_el = $("#height_el1").val();
+                var div_el = "";
+                if (click_wstaw == 1) {
+                    div_el = "#1_div";
+                    check_element();
+                }
+                if (click_wstaw == 2) {
+                    div_el = "#2_div";
+                    check_element();
+                }
+    function check_element () {
+                if (elements == "1") {
+                   $(div_el).append('<img id="theImg" src="image/rectangle.png"/>');
+                    //$(div_el).text("element");
+                    $("#theImg").css({
+                        "width": val1_el/6 + "vw",
+                        "height": val2_el/5 + "vh"
+                    });
+                    //$(div_el).append("<span class='span_left'>1</span>");
+                    //$(div_el).append("<span class='span_down'>2</span>");
+                    $(".span_left").text(val1_el);
+                    $(".span_down").text(val2_el);
+                    alert(click_wstaw);
+                }
+                else {
+                    $(div_el).append('<img src="image/squ.png"/>');
+                }
+    };
+            };
